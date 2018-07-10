14baa45f3678a6df2c7c79b5f864ff0c7b8d51e8
Bernhard Rubow <b.rubow@t-online.de>
2018-07-10 12:32:35 +0200

    Working login

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,25 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Nakama;
 
 public class NetworkManager : MonoBehaviour
 {
+    // +++ events +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public ObjectEventDelegate OnMatchCreated = delegate {};
+    public ObjectEventDelegate OnDebugMessage = delegate {};
+
+
+
 
     // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Nakama.Client _client;
     ISession _session;
     IApiAccount _account;
     ISocket _socket;
+    IMatch _match;
+    [SerializeField] bool _initMatch;
+    [SerializeField] UiManager _ui;
+    private List<IUserPresence> _presences = new List<IUserPresence>();
 
-    // Use this for initialization
+
+
+
+    // +++ unity lifecycle ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     async void Start()
     {
         _client = new Client("defaultkey", "aws.mkwindweb2.de", 7350, false);
 
-        const string email = "hello@example.com";
-        const string password = "somesupersecretpassword";
-        _session = await _client.AuthenticateEmailAsync(email, password);
+        // const string email = "hello@example.com";
+        // const string password = "somesupersecretpassword";
+        // _session = await _client.AuthenticateEmailAsync(email, password);
+        _session = await _client.AuthenticateDeviceAsync(System.Guid.NewGuid().ToString());
         Debug.LogFormat("Authenticated session: {0}", _session);
         Debug.Log(_session.AuthToken); // raw JWT token
         Debug.LogFormat("User id '{0}'", _session.UserId);
@@ -46,71 +61,113 @@ public class NetworkManager : MonoBehaviour
         _socket.OnStreamPresence += _socket_OnStreamPresence;
         _socket.OnStreamState += _socket_OnStreamState;
         await _socket.ConnectAsync(_session);
+
+        // register ui events 
+        _ui.OnCreateMatch += OnUiCreateMatch;
+        _ui.OnJoinMatch += OnUiJoinMatch;
+
+
+        
     }
 
-    private void _socket_OnStreamState(object sender, IStreamState e)
-    {
-        throw new System.NotImplementedException();
-    }
 
-    private void _socket_OnStreamPresence(object sender, IStreamPresenceEvent e)
-    {
-        throw new System.NotImplementedException();
-    }
 
-    private void _socket_OnStatusPresence(object sender, IStatusPresenceEvent e)
-    {
-        throw new System.NotImplementedException();
-    }
 
-    private void _socket_OnNotification(object sender, IApiNotification e)
+    // +++ used event handlers ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void _socket_OnConnect(object sender, System.EventArgs e)
     {
-        throw new System.NotImplementedException();
-    }
-
-    private void _socket_OnMatchState(object sender, IMatchState e)
-    {
-        throw new System.NotImplementedException();
+        this.OnDebugMessage("Connected to Nakama Multiplayer Server");
     }
 
     private void _socket_OnMatchPresence(object sender, IMatchPresenceEvent e)
     {
-        throw new System.NotImplementedException();
+        this.OnDebugMessage("Presences in match have changed!");
+
+        foreach(var p in e.Joins){
+            this.OnDebugMessage(string.Format("Player {0} joins", p.UserId));
+            if(_presences.Count(x => x.UserId == p.UserId) == 0)
+                _presences.Add(p);
+        }        
+        List_Players(_presences);  
+    }
+
+    private async void OnUiCreateMatch(object eventArgs){
+        _match = await _socket.CreateMatchAsync();
+        this.OnDebugMessage(string.Format("Created match with ID: {0}", _match.Id));
+        this.OnMatchCreated(_match.Id);
+    }
+
+    private async void OnUiJoinMatch(object eventArgs){
+        this.OnDebugMessage("Joining match...");
+        string matchId = eventArgs.ToString();
+        _match = await _socket.JoinMatchAsync(matchId);
+        foreach(var p in _match.Presences) _presences.Add(p);
+        List_Players(_presences);
+    }
+
+
+
+
+    // +++ unused eventhandlers (so far) ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    private void _socket_OnStreamState(object sender, IStreamState e)
+    {
+        Debug.Log("Call to socket_OnStreamState");
+    }
+
+    private void _socket_OnStreamPresence(object sender, IStreamPresenceEvent e)
+    {
+        Debug.Log("Call to socket_OnStreamPresence");
+    }
+
+    private void _socket_OnStatusPresence(object sender, IStatusPresenceEvent e)
+    {
+        Debug.Log("Call to socket_OnStatusPresence");
+    }
+
+    private void _socket_OnNotification(object sender, IApiNotification e)
+    {
+        Debug.Log("Call to socket_OnNotification");
+    }
+
+    private void _socket_OnMatchState(object sender, IMatchState e)
+    {
+        Debug.Log("Call to socket_OnMatchState");
     }
 
     private void _socket_OnMatchmakerMatched(object sender, IMatchmakerMatched e)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Call to socket_OnMatchmakerMatched");
     }
 
     private void _socket_OnError(object sender, System.Exception e)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Call to socket_OnError");
     }
 
     private void _socket_OnChannelPresence(object sender, IChannelPresenceEvent e)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Call to socket_OnChannelPresence");
     }
 
     private void _socket_OnChannelMessage(object sender, IApiChannelMessage e)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Call to socket_OnChannelMessage");
     }
 
     private void _socket_OnDisconnect(object sender, System.EventArgs e)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Call to socket_OnDisconnect");
     }
 
-    private void _socket_OnConnect(object sender, System.EventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+
+    // +++ functions ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void List_Players(List<IUserPresence> presences){
+        this.OnDebugMessage("Match Joined, listing players:");        
+        foreach(var p in presences){
+            this.OnDebugMessage(string.Format("--- {0}", p.UserId));
+        }
     }
 }
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Nakama;
+
+public class UiManager : MonoBehaviour {
+
+	// +++ events exposed +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	public ObjectEventDelegate OnCreateMatch = delegate {};
+	public ObjectEventDelegate OnJoinMatch = delegate {};
+
+
+
+
+	// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	[SerializeField] private NetworkManager _networkManager;
+	[SerializeField] private InputField _matchIdDisplay;
+	[SerializeField] private InputField _matchIdToJoin;
+	[SerializeField] private Text _debugText;
+	private string _textToAppend;
+	
+
+
+
+
+	// +++ unity life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void Start () {
+		_debugText.text = "";
+		_networkManager.OnMatchCreated +=	OnMatchCreated;
+		_networkManager.OnDebugMessage += OnDebugMessageReceived;
+	}
+
+
+	void Update()
+	{
+			if(!string.IsNullOrEmpty(_textToAppend)){
+				_debugText.text += _textToAppend;
+				_textToAppend = string.Empty;
+			}
+	}
+
+
+
+	void Reset(){
+		_networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+	}
+
+	// +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void OnMatchCreated(object eventArgs){		
+		_matchIdDisplay.text = eventArgs.ToString();
+	}
+
+	void OnDebugMessageReceived(object eventArgs){
+		_textToAppend += "\n" + eventArgs.ToString();
+	}
+
+	public void OnCreateMatchClicked(){
+		this.OnCreateMatch(null);
+	}
+
+	public void OnJoinMatchClicked(){
+		this.OnJoinMatch(_matchIdToJoin.text);
+	}
+
+	
+
+}
